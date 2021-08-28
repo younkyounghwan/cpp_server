af54b04f4de70b5c37277ec16fd1d22973805562
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-08-29 01:49:51 +0900

    21-08-29

---
+++ Lock-Based Queue/GameServer/GameServer.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <Windows.h>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
+
+LockQueue<int32> q;
+LockStack<int32> s;
+
+void Push()
+{
+	while (true)
+	{
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		//if (q.Empty())
+		//	continue;
+		
+		int32 data = 0;
+		if(q.TryPop(OUT data))		
+			cout << data << endl;
+	}
+}
+
+int main()
+{
+
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
+}
