b919f238259d4673153cbde716b5a0b7c68ca09a
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2022-01-13 23:52:41 +0900

    22-01-13

---
+++ Lock-free Queue/GameServer/ConcurrentQueue.h
@@ -0,0 +1,283 @@
+#pragma once
+#include <mutex>
+
+template<typename T>
+
+class LockQueue
+{
+public:
+	LockQueue() {}
+
+	LockQueue(const LockQueue&) = delete;
+	LockQueue& operator = (const LockQueue&) = delete;
+
+	void Push(T value)
+	{
+		lock_guard<mutex> lock(_mutex);
+		_queue.push(std::move(value));
+		_condVar.notify_one();
+	}
+
+	bool TryPop(T& value)
+	{
+		lock_guard<mutex> lock(_mutex);
+		if (_queue.empty())
+			return false;
+
+		// empty -> top -> pop
+		value = std::move(_queue.front());
+		_queue.pop();
+		return true;
+	}
+
+	bool Empty()
+	{
+		lock_guard<mutex> lock(_mutex);
+		return _queue.empty();
+	}
+
+	void WaitPop(T& value)
+	{
+		unique_lock<mutex> lock(_mutex);
+		_condVar.wait(lock, [this] {return _queue.empty() == false; });
+		value = std::move(_queue.front());
+		_queue.pop();
+	}
+
+private:
+	queue<T> _queue;
+	mutex _mutex;
+	condition_variable _condVar;
+
+};
+
+//template <typename T> 
+//class LockFreeQueue
+//{
+//	struct Node
+//	{
+//		shared_ptr<T> data;
+//		Node* next = nullptr;
+//	};
+//
+//public:
+//	LockFreeQueue() : _head(new Node), _tail(_head)
+//	{
+//
+//	}
+//
+//
+//	LockFreeQueue(const LockFreeQueue&) = delete;
+//	LockFreeQueue& operator = (const LockFreeQueue&) = delete;
+//
+//	void Push(const T& value)
+//	{
+//		sharded_ptr<T> newData = make_shared<T>(value);
+//
+//		Node* dummy = new Node();
+//
+//		Node* oldTail = _tail;
+//		oldTail->data.swap(newData);
+//		oldTail->next = dummy;
+//
+//		_tail = dummy;
+//	}
+//
+//	shared_ptr<T> TryPop()
+//	{
+//		Node* oldeHead = PopHead();
+//		if (oldHead == nullptr)
+//			return shared_ptr<T>();
+//		shared_ptr<T> res(oldHead->data);
+//		delete oldHead;
+//		return res;
+//	}
+//
+//private:
+//	Node* PopHead()
+//	{ // head에 있는 함수를 가져온다.
+//		Node* oldHead = _head;
+//		if (oldHead == _tail)
+//			return nullptr;
+//
+//		_head = oldHead->next;
+//		return oldHead;
+//	}
+//
+//
+//
+//	// [ ]
+//	// [head[tail] head와 tail이 같은 노드를 가리키고 있으면 그 노드는 빈 노드이다.
+//	Node* _head = nullptr;
+//	Node* _tail = nullptr;
+//
+//	
+//};
+
+template <typename T>
+class LockFreeQueue
+{
+	struct Node;
+
+	struct CountedNodePtr
+	{
+		int32 externalCount; // 참조권
+		Node* ptr = nullptr;
+	};
+
+	struct NodeCounter 
+	{
+		uint32 internalCount : 30; // 참조권 반환 관련
+		uint32 externalCountRemaining : 2; // Push & Pop 다중 참조권 관련
+	};
+
+	struct Node
+	{
+		Node()
+		{
+			NodeCounter newCount;
+			newCount.internalCount = 0;
+			newCount.externalCountRemaining = 2;
+			count.store(newCount);
+
+			next.ptr = nullptr;
+			next.externalCount = 0;
+		}
+
+		void ReleaseRef()
+		{
+			NodeCounter oldCounter = count.load();
+
+			while (true)
+			{
+				NodeCounter newCounter = oldCounter;
+				newCounter.internalCount--;
+
+				//끼어들 수도 있음
+				if (count.compare_exchange_strong(oldCounter, newCounter))
+				{
+					if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+						delete this;
+				}
+			}
+
+			atomic<T*> data;
+			atomic<NodeCounter> count;
+			CountedNodePtr next;
+		};
+	}
+
+public:
+	LockFreeQueue() : _head(new Node), _tail(_head)
+	{
+
+	}
+
+
+	LockFreeQueue(const LockFreeQueue&) = delete;
+	LockFreeQueue& operator = (const LockFreeQueue&) = delete;
+
+	// [data][data][value][]
+	// [head][tail]
+
+	void Push(const T& value)
+	{
+		unique_ptr<T> newData = make_unique<T>(value);
+
+		CountedNodePtr dummy;
+		dummy.ptr = new Node;
+		dummy.externalCount = 1;
+
+		CountedNodePtr oldTail = _tail.load(); //ptr = nullptr
+
+		while (true)
+		{
+			// 참조권 획득 (externalCount를 현시점 기준 +1한 애가 이김)
+			IncreaseExternalCount(_tail, oldTail);
+
+			// 소유권 획득 (data를 먼저 교환한 애가 이김)
+			T* oldData = nullptr;
+			if (oldTail.ptr->data.compare_exchange_strong(oldData, newData->get()))
+			{
+				oldTail.ptr->next = dummy;
+				oldTail = _tail.exchange(dummy);
+				FreeExternalCount(oldTail);
+				newData.release(); // .데이터에 대한 unique_ptr의 고유권의 포기
+				break;
+			}
+
+			// 소유권 경쟁 패배
+			oldTail.ptr->ReleaseRef();
+
+		}
+
+		//sharded_ptr<T> newData = make_shared<T>(value);
+
+		//Node* dummy = new Node();
+
+		//Node* oldTail = _tail;
+		//oldTail->data.swap(newData);
+		//oldTail->next = dummy;
+
+		//_tail = dummy;
+	}
+
+	shared_ptr<T> TryPop()
+	{
+		//Node* oldeHead = PopHead();
+		//if (oldHead == nullptr)
+		//	return shared_ptr<T>();
+		//shared_ptr<T> res(oldHead->data);
+		//delete oldHead;
+		//return res;
+	}
+
+private:
+	static void IncreaseExternalCount(atomic <CountedNodePtr>& counter, CountedNodePtr& oldCounter)
+	{
+		while (true)
+		{
+			CountedNodePtr newCounter = oldCounter;
+			newCounter.externalCount++;
+
+			if (counter.compare_exchange_strong(oldCounter, newCounter))
+			{
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
+
+		}
+	}
+
+	static void FreeExternalCount(CountedNodePtr& oldNodePtr)
+	{
+		Node* ptr = oldNodePtr.ptr;
+		const int32 countIncrease = oldNodePtr.externalCount - 2;
+
+		NodeCounter oldCounter = ptr->count.load();
+
+		while (true)
+		{
+			NodeCounter newCounter = oldCounter;
+			newCounter.externalCountRemaining--; // TODO
+			newCounter.internalCount += countIncrease;
+
+			if (ptr->count.compare_exchange_strong(oldCounter, newCounter))
+			{
+				if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+					delete;
+
+				break;
+			}
+		}
+	}
+
+
+	// [ ]
+	// [head[tail] head와 tail이 같은 노드를 가리키고 있으면 그 노드는 빈 노드이다.
+
+	atomic <CountedNodePtr> _head;
+	atomic <CountedNodePtr> _tail;
+
+
+};
---
+++ Lock-free Queue/GameServer/GameServer.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <Windows.h>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
+
+LockQueue<int32> q;
+LockFreeStack<int32> s;
+
+void Push()
+{
+	while (true)
+	{
+		int32 value = rand() % 100;
+		s.Push(value);
+
+		//this_thread::sleep_for(10ms);
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		auto data = s.TryPop();
+		if (data != nullptr)
+			cout << (*data) << endl;
+
+	}
+}
+
+int main()
+{
+	shared_ptr<int32> ptr;
+	bool value = atomic_is_lock_free(&ptr);
+
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
+}
