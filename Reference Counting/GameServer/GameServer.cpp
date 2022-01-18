4a22bb9c57e58faed52f769d7a93d55d795ee3a5
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2022-01-18 22:57:09 +0900

    22-01-18

--- Reference Counting/GameServer/GameServer.cpp
+++ Reference Counting/GameServer/GameServer.cpp
@@ -7,58 +7,84 @@
 #include <future>
 #include <Windows.h>
 #include "ThreadManager.h"
+#include "RefCounting.h"
 
-class Wraight
+class Wraight : public RefCountable
 {
 public:
 	int _hp = 150;
 	int _posX = 0;
 	int _posY = 0;
-
-
 };
+using WraightRef = TSharedPtr<Wraight>;
 
-class Missile
+class Missile : public RefCountable
 {
 public:
-	void SetTarget(Wraight* target)
+	void SetTarget(WraightRef target)
 	{
 		_target = target;
+		// 중간에 누군가 개입할 수 있음
+		//target->AddRef();
+		Test(target);
 	}
 
-	void Update()
+	void Test(WraightRef& target) 
+	{// 내부적으로만 쓸거라면 reference를 사용해서 refCount를 아끼자.
+
+	}
+
+	bool Update()
 	{
+		if (_target == nullptr)
+			return true;
+
 		int _posX = _target->_posX;
 		int _posY = _target->_posY;
+
+		// TODO: 쫒아간다.
+	
+		if (_target->_hp == 0)
+		{
+			_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+
+		return false;
 	}
 
-	// TODO: 쫒아간다.
-
-
-	Wraight* _target = nullptr;
-
-
+	WraightRef _target = nullptr;
 };
+
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	Wraight* wraight = new Wraight();
-	Missile* missile = new Missile();
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
+	
 	missile->SetTarget(wraight);
 
 	//레이스가 피격 당함
 	wraight->_hp = 0;
-	delete wraight;
-
+	//delete wraight;
+	//wraight->ReleaseRef();
+	wraight = nullptr;
 
 	while (true)
 	{
-		if (missile)
+		if (missile->Update())
 		{
-			missile->Update();
-
+			//missile->ReleaseRef();
+			missile = nullptr;
 		}
 	}
 
-
+	//missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
 }
