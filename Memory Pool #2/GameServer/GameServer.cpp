84111de5ca3f1377b46cb9a04f15be3e8281cf63
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2022-01-22 22:17:57 +0900

    22-01-22

---
+++ Memory Pool #2/GameServer/GameServer.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <Windows.h>
+#include "ThreadManager.h"
+
+#include "RefCounting.h"
+#include "Memory.h"
+#include "Allocator.h"
+
+
+class Player
+{
+public:
+	Player() {}
+	virtual ~Player() {}
+};
+
+class Knight : public Player
+{
+public:
+	Knight()
+	{
+		cout << "Knight()" << endl;
+	}
+
+	Knight(int32 hp) : _hp(hp) 
+	{
+		cout << "Knight(hp)" << endl;
+	}
+
+	~Knight()
+	{
+		cout << "~Knight()" << endl;
+	}
+
+	int32 _hp = 100;
+	int32 _mp = 10;
+};
+
+int main()
+{
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					//[ ][ ][ ][ ][ ][ ][ ][ ][ ]
+					// [             [        ] ]
+					xvector<Knight> v(10);
+
+					xmap<int32, Knight> m;
+					m[100] = Knight();
+
+					this_thread::sleep_for(10ms);
+				}
+			});
+	}
+
+	GThreadManager->Join();
+
+}
