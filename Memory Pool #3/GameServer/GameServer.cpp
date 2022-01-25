ece6dccfde1116b334497bc2250f173674669a2c
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2022-01-25 22:37:34 +0900

    22-01-25

---
+++ Memory Pool #3/GameServer/GameServer.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <Windows.h>
+#include "ThreadManager.h"
+
+#include "RefCounting.h"
+#include "Memory.h"
+#include "Allocator.h"
+#include "LockFreeStack.h"
+
+
+DECLSPEC_ALIGN(16)
+class Data // : public SListEntry 
+{
+public:
+	SListEntry _entry;
+
+	int64 _rand = rand() % 1000;
+};
+SListHeader* GHeader;
+int main()
+{
+	GHeader = new SListHeader();
+	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
+
+	InitializeHead(GHeader);
+
+	for (int32 i = 0; i < 3; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Data* data = new Data();
+					ASSERT_CRASH(((uint64)data % 16) == 0);
+
+
+					PushEntrySList(GHeader, (SListEntry*)data);
+					this_thread::sleep_for(10ms);
+				}
+			});
+	}
+
+	for (int32 i = 0; i < 2; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Data* pop = nullptr;
+					pop = (Data*)PopEntrySList(GHeader);
+
+					if (pop)
+					{
+						cout << pop->_rand << endl;
+						delete pop;
+					}
+					else
+					{
+						cout << "None" << endl;
+					}
+				}
+			});
+	}
+	GThreadManager->Join();
+
+}
